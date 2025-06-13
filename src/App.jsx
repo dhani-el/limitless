727f1572bf020e3d34d59070a51e584e2eb4db5b
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2025-06-13 15:10:47 +0100

    switch vr start tactics

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import { Canvas } from '@react-three/fiber'
 import { XR, createXRStore, } from '@react-three/xr'
-import { useState,useEffect } from 'react'
+import { useState,useEffect, useRef } from 'react'
 
 const store = createXRStore();
 
 export default function App() {
   const [red, setRed] = useState(false);
+  const buttonRef = useRef(null);
 
   useEffect(function(){
-    store.enterVR()
+    // if (buttonRef.current != null) {
+    //   buttonRef.current?.click?.()
+    // }
+
   },[])
 
   return <>
-    <button onClick={() =>store.enterVR() }>Enter AR</button>
-    <Canvas>
+    <button ref={buttonRef} onClick={() =>store.enterVR() }>Enter AR</button>
+    <Canvas onLoad={()=>{store.enterVR()}} >
       <XR store={store}>
         <mesh pointerEventsType={{ deny: 'grab' }} onClick={() => setRed(!red)} position={[0, 1, -1]}>
           <boxGeometry />
