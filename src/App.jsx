9ffc989563d3b4dc1eb8996578af37471dedcebd
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2025-06-13 16:13:34 +0100

    patch for instant vr

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,26 @@ import { Canvas } from '@react-three/fiber'
 import { XR, createXRStore, } from '@react-three/xr'
 import { useState,useEffect, useRef } from 'react'
 
-const store = createXRStore();
 
+const store = createXRStore();
 export default function App() {
   const [red, setRed] = useState(false);
   const buttonRef = useRef(null);
+  const canvasRef = useRef(null);
+  const xrRef = useRef(null);
 
-  useEffect(function(){
-    // if (buttonRef.current != null) {
-    //   buttonRef.current?.click?.()
-    // }
-
-  },[])
+useEffect(function(){
+  if (canvasRef.current != null  && xrRef.current != null) {
+    console.log("canvas has loaded");
+    store.enterVR()
+  }
+},[canvasRef,xrRef])
 
   return <>
-    <button ref={buttonRef} onClick={() =>store.enterVR() }>Enter AR</button>
-    <Canvas onLoad={()=>{store.enterVR()}} >
-      <XR store={store}>
-        <mesh pointerEventsType={{ deny: 'grab' }} onClick={() => setRed(!red)} position={[0, 1, -1]}>
+    <button ref={buttonRef} onClick={() =>store.enterVR() }>Enter VR</button>
+    <Canvas  ref={canvasRef}  >
+      <XR  store={store}>
+        <mesh ref={xrRef} pointerEventsType={{ deny: 'grab' }} onClick={() => setRed(!red)} position={[0, 1, -1]}>
           <boxGeometry />
           <meshBasicMaterial color={red ? 'red' : 'blue'} />
         </mesh>
