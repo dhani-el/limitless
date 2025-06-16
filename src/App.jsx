624c7dd19561b117811c0f49f100dfc77c0f7162
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2025-06-16 21:09:40 +0100

    re work on the floor

--- src/App.jsx
+++ src/App.jsx
@@ -10,24 +10,28 @@ const store = createXRStore({
 export default function App() {
   const [red, setRed] = useState(false);
 
-
-
-
   return <>
     <button  onClick={() =>store.enterVR() }>Enter VR</button>
     <Canvas  >
       <XR   store={store}>
-
-        <mesh  pointerEventsType={{ deny: 'grab' }} onClick={() => setRed(!red)} position={[0, 1, -1]}>
-          <boxGeometry />
-          <meshBasicMaterial color={red ? 'red' : 'blue'} />
-        </mesh>
-
-        <mesh rotateZ={90} >
-          <planeGeometry  scale={[5,10]} />
-          <meshBasicMaterial color={"white"} />
-        </mesh>
+        <Box/>
+        <Floor/>
       </XR>
     </Canvas>
   </>
+}
+
+
+function Floor(){
+  return  <mesh rotateX={-Math.PI / 2} >
+              <planeGeometry  scale={[6,6]} />
+              <meshBasicMaterial color={"white"} />
+          </mesh>
+}
+
+function Box(){
+  return <mesh  pointerEventsType={{ deny: 'grab' }} onClick={() => setRed(!red)} position={[0, 1, -1]}>
+            <boxGeometry  scale={[1,1,1]} />
+            <meshBasicMaterial color={red ? 'red' : 'blue'} />
+        </mesh>
 }
