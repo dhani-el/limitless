5ee5a412400069cfcab3e1b8d9222d3a73e74cb7
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2025-10-01 14:53:05 +0100

    stable implmentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
-import { Canvas,useLoader } from '@react-three/fiber'
-import { DefaultXRController, XR, createXRStore,useXRInputSourceStateContext } from '@react-three/xr'
+import { Canvas,useFrame,useLoader } from '@react-three/fiber'
+import { XR,VRButton,useXR,Controllers,Hands,useController} from '@react-three/xr'
 import { useState,useEffect, useRef } from 'react'
 import { DoubleSide } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { Environment, Clone } from '@react-three/drei';
+import { Environment, Clone,Line,OrbitControls } from '@react-three/drei';
 import "./index.css"
 
-const store = createXRStore({
-  layers:false,
-  hand:{rayPointer:{rayModel:{color:"red"}}},
-});
 
 export default function App() {
+
   return <>
-    <button  onClick={() =>store.enterVR() }>Enter VR</button>
+    <VRButton/>
     <Canvas  >
-      <XR  store={store}>
+      <XR>
+        <Environment preset='warehouse' />
         <ambientLight intensity={1} />
         <directionalLight color="white" position={[0, 1, 0]} />
+        {/* <Controllers/> */}
+        <CustomController handedness={"left"}/>
+        <CustomController handedness={"right"}/>
+        <Hands/>
         <Floor/>
         <Targets />
+        <OrbitControls/>
       </XR>
     </Canvas>
   </>
@@ -58,4 +61,29 @@ function Targets({amount=[1,2,3,4],}){
               })
             }
   </>
+}
+
+function CustomController({ handedness }) {
+  const  controller  = useController(handedness); // "left" or "right"
+  const model = useLoader(GLTFLoader,'blaster.glb'); // replace with your file
+  const ref = useRef(null);
+
+  useFrame(() => {
+    if (controller && ref.current) {
+      // Copy position & rotation from XR controller grip
+      ref.current.position.copy(controller.grip.position)
+      ref.current.quaternion.copy(controller.grip.quaternion)
+    }
+  })
+
+  if (!controller) return null
+
+  return (
+    <Clone
+      ref={ref}
+      object={model.scene}
+      position={controller.grip.position}
+      quaternion={controller.grip.quaternion}
+    />
+  )
 }
